32eab619e8153d681a0be243afedf5661ded7d74
codingisfun <61808280+jennyq7@users.noreply.github.com>
2022-12-06 13:05:50 -0500

    work on header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import {BrowserRouter, Routes, Route } from 'react-router-dom'; 
 import './App.scss';
 import HomePage from './pages/HomePage/HomePage.js';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        {/* <Header /> */}
+        <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/recipes/:id" />
@@ -15,6 +16,6 @@ function App() {
       </BrowserRouter>
     </div>
   );
-} 
+}; 
 
 export default App;
