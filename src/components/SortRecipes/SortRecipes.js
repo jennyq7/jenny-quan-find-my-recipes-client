b588364381a8e7536554bff2cce1da13c046f66d
codingisfun <61808280+jennyq7@users.noreply.github.com>
2022-12-06 22:47:22 -0500

    work on sort functionality

--- src/components/SortRecipes/SortRecipes.js
+++ src/components/SortRecipes/SortRecipes.js
@@ -1,18 +1,26 @@
 import './SortRecipes.scss';
 
 
-function SortRecipes () {
+function SortRecipes ({recipes}) {
 
-function handleSort () {
-
+function sortName () {
+    recipes.sort((a, b) => {
+       if( a.recipe_name > b.recipe_name ) {
+        return 1;
+       } else if (a.recipe_name < b.recipe_name) {return -1;}
+    return 0;
+    }
+    )
 }
 
  return (
     <div className="sort">
-        <label htmlFor="sort"></label>
+        <label htmlFor="sort">Sort</label>
         <select id="sort" name="sort">
+            <option value="start">Choose</option>
             <option value="category">Category</option>
-            <option value="name">Name</option>
+            <option value="name" onClick={sortName}>Name</option>
+            <option value="number-of-ingredients">Number of ingredients</option>
         </select>
     </div>
  )
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -7,7 +7,7 @@ function HomePage () {
 
   const [recipes, setRecipes] = useState(null);
 
-//to get recipe: response.data.recipe[0]
+
 
   useEffect(()=> {axios.get('http://localhost:8080/recipes')
   .then(response => {
@@ -21,20 +21,22 @@ function HomePage () {
     
     if(!recipes) {return (<p>Loading....</p>) };
 
+
+
     return (
         <main className="main">
             <h3>Welcome to my site!</h3>
             <p>Feel free to browse the below recipes or use the sort or filter options to search by your desired criteria</p>
             <div className="main__buttons">
-              <SortRecipes />
-              
+              <SortRecipes recipes={recipes}/>
+              <button>Randomize</button>
             </div>
             <div className="main__display">
      {recipes.map(recipe => {
         return (
             <div className="main__display-item" key={recipe.recipe_id}>        
                <h4>{recipe.recipe_name}</h4>
-               <img src={recipe.recipe_image} alt="recipe"/>
+               <img src={recipe.recipe_image} alt="recipe" className="main__display-item-img"/>
                <p>{"Category: " + recipe.recipe_types.recipe_type[0]}</p>
                <p>{"Number of ingredients: " + (recipe.recipe_ingredients.ingredient).length}</p>
             </div> 
