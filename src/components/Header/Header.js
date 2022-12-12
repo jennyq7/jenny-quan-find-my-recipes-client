6d6d3217930bde79887b948cd5867bfb3c2cb691
codingisfun <61808280+jennyq7@users.noreply.github.com>
2022-12-12 10:47:18 -0500

    fix functions in post and save and styling on head

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 function Header () {
     return (
         <header className="header">
-            <Link to="/recipes"><h1>FIND MY RECIPES</h1></Link>
+            <Link to="/recipes" className="header__link"><h1 className="header__title">FIND MY RECIPES</h1></Link>
         </header>
     )
 };
