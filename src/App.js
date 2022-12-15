88497f7d0acc57effd4dbba1ef5b809a3a3e429a
codingisfun <61808280+jennyq7@users.noreply.github.com>
2022-12-14 22:03:06 -0500

    formatted app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
 import HomePage from './pages/HomePage/HomePage.js';
 import Header from './components/Header/Header';
@@ -14,14 +14,14 @@ function App() {
         <Header />
         <Routes>
           <Route path="/recipes" element={<HomePage />} />
-          <Route path="/recipes/:id" element={<Recipe />}/>
+          <Route path="/recipes/:id" element={<Recipe />} />
           <Route path="/recipes/add" element={<AddRecipe />} />
-          <Route path="/recipes/saved" element={<SavedRecipes />}/>
+          <Route path="/recipes/saved" element={<SavedRecipes />} />
         </Routes>
         <Footer />
       </BrowserRouter>
     </div>
   );
-}; 
+};
 
 export default App;
